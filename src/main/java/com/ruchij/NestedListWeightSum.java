cc0bef557db86e385a4d4cdf8dfc6f3f2d40ad09
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-03-09 18:19:15 +1100

    Solutions for NestedListWeightSum

---
+++ src/main/java/com/ruchij/NestedListWeightSum.java
@@ -0,0 +1,72 @@
+package com.ruchij;
+
+import java.util.List;
+
+public class NestedListWeightSum {
+      // This is the interface that allows for creating nested lists.
+      // You should not implement it, or speculate about its implementation
+      public interface NestedInteger {
+          // @return true if this NestedInteger holds a single integer, rather than a nested list.
+          public boolean isInteger();
+
+          // @return the single integer that this NestedInteger holds, if it holds a single integer
+          // Return null if this NestedInteger holds a nested list
+          public Integer getInteger();
+
+          // Set this NestedInteger to hold a single integer.
+          public void setInteger(int value);
+
+          // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+          public void add(NestedInteger ni);
+
+          // @return the nested list that this NestedInteger holds, if it holds a nested list
+          // Return empty list if this NestedInteger holds a single integer
+          public List<NestedInteger> getList();
+      }
+
+    public int depthSum(List<NestedInteger> nestedList) {
+        NestedInteger nestedInteger =
+                new NestedInteger() {
+                    @Override
+                    public boolean isInteger() {
+                        return false;
+                    }
+
+                    @Override
+                    public Integer getInteger() {
+                        return null;
+                    }
+
+                    @Override
+                    public void setInteger(int value) {
+
+                    }
+
+                    @Override
+                    public void add(NestedInteger ni) {
+
+                    }
+
+                    @Override
+                    public List<NestedInteger> getList() {
+                        return nestedList;
+                    }
+                };
+
+        return sum(nestedInteger, 0);
+    }
+
+    private int sum(NestedInteger nestedInteger, int level) {
+          if (nestedInteger.isInteger()) {
+              return level * nestedInteger.getInteger();
+          } else {
+              int result = 0;
+
+              for (NestedInteger integer : nestedInteger.getList()) {
+                  result += sum(integer, level + 1);
+              }
+
+              return result;
+          }
+    }
+}

43c2f092a2a2092c78fbe2d031af61fdd1d5ca1f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-04-10 13:32:24 +1000

    Updated solution for CopyListWithRandomPointer

--- src/main/java/com/ruchij/CopyListWithRandomPointer.java
+++ src/main/java/com/ruchij/CopyListWithRandomPointer.java
@@ -1,5 +1,8 @@
 package com.ruchij;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopyListWithRandomPointer {
     static class Node {
         int val;
@@ -23,47 +26,29 @@ public class CopyListWithRandomPointer {
     }
 
     public Node copyRandomList(Node input) {
-        Node node = new Node(-1);
-        Node head = input;
-        Node current = node;
-
-        while (head != null) {
-            current.next = new Node(head.val);
-            current = current.next;
-            head = head.next;
-        }
-
-        head = input;
-        current = node.next;
-
-        while (head != null) {
-            Node random = head.random;
-
-            if (random != null) {
-                Node original = input;
-                int index = 0;
-
-                while (original != random) {
-                    original = original.next;
-                    index++;
-                }
-
-                int currentIndex = 0;
-
-                Node updated = node.next;
-
-                while (currentIndex != index) {
-                    updated = updated.next;
-                    currentIndex++;
-                }
-
-                current.random = updated;
-            }
-
-            head = head.next;
-            current = current.next;
-        }
-
-        return node.next;
+        return copyRandomList(input, new HashMap<>());
     }
+
+    private Node copyRandomList(Node input, Map<Node, Node> visited) {
+        if (input == null) {
+            return null;
+        } else {
+            Node existing = visited.get(input);
+
+            if (existing != null) {
+                return existing;
+            } else {
+                Node node = new Node(input.val);
+
+                visited.put(input, node);
+
+                node.next = copyRandomList(input.next, visited);
+                node.random = copyRandomList(input.random, visited);
+
+                return node;
+            }
+        }
+    }
+
+
 }

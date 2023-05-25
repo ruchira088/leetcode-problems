8b43e37c1016878ee9e8f900e73742a356fc8605
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-05-25 20:10:46 +1000

    Solutions for RunningSumOfArray

---
+++ src/main/java/com/ruchij/RunningSumOfArray.java
@@ -0,0 +1,15 @@
+package com.ruchij;
+
+public class RunningSumOfArray {
+    public int[] runningSum(int[] nums) {
+        int[] sums = new int[nums.length];
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sums[i] = sum;
+        }
+
+        return sums;
+    }
+}

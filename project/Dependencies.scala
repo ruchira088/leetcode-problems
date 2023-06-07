684ec4d5ae3f1470d55d8b034c305692f320fa79
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-06-07 18:23:13 +1000

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies
 {
-  val ScalaVersion = "2.13.10"
+  val ScalaVersion = "2.13.11"
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.13.2" cross CrossVersion.full
 
   lazy val junitJupiterApi = "org.junit.jupiter" % "junit-jupiter-api" % "5.9.3"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }

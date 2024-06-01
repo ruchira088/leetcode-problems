dd36d5e9cd0ee557178a8d122d98a3c392c27291
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-06-01 17:05:20 +1000

    Complete solution for UniquePaths

--- src/main/java/com/ruchij/UniquePaths.java
+++ src/main/java/com/ruchij/UniquePaths.java
@@ -1,34 +1,31 @@
 package com.ruchij;
 
-import java.util.ArrayDeque;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UniquePaths {
     record Coordinate(int x, int y) {}
 
     public int uniquePaths(int m, int n) {
-        Coordinate start = new Coordinate(0, 0);
-        Coordinate destination = new Coordinate(n - 1, m - 1);
-        int count = 0;
-        ArrayDeque<Coordinate> queue = new ArrayDeque<>();
-        queue.add(start);
+        Map<Coordinate, Integer> cache = new HashMap<>();
 
-        while (!queue.isEmpty()) {
-            Coordinate current = queue.poll();
-
-            if (current.equals(destination)) {
-                count++;
-            } else {
-                Stream.of(new Coordinate(current.x + 1, current.y), new Coordinate(current.x, current.y + 1))
-                        .filter(coordinate -> isValid(m, n, coordinate))
-                        .forEach(queue::add);
-            }
-        }
-
-        return count;
+        return count(n - 1, m - 1, cache);
     }
 
-    boolean isValid(int m, int n, Coordinate coordinate) {
-        return coordinate.x < n && coordinate.y < m;
+    private int count(int x, int y, Map<Coordinate, Integer> cache) {
+        Integer result = cache.get(new Coordinate(x, y));
+
+        if (result != null) {
+            return result;
+        } else if (x < 0 || y < 0) {
+            return 0;
+        } else if (x == 0 && y == 0) {
+            return 1;
+        } else {
+            int answer = count(x - 1, y, cache) + count(x, y - 1, cache);
+            cache.put(new Coordinate(x, y), answer);
+            cache.put(new Coordinate(y, x), answer);
+            return answer;
+        }
     }
 }

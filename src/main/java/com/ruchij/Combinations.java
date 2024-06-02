d0950caf3b83244789c7438459d6f4874b7e0911
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-06-02 20:30:42 +1000

    Updated solution for Combinations

--- src/main/java/com/ruchij/Combinations.java
+++ src/main/java/com/ruchij/Combinations.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combinations {
+    record Entry(List<Integer> integers, int size) {}
+
     public List<List<Integer>> combine(int n, int k) {
-        return combine(n, k, new ArrayList<>(List.of(new ArrayList<>())), new ArrayList<>());
+        return combine(n, k, new ArrayList<>(List.of(new Entry(new ArrayList<>(), 0))), new ArrayList<>());
     }
 
-    private List<List<Integer>> combine(int n, int k, List<List<Integer>> current, List<List<Integer>> results) {
+    private List<List<Integer>> combine(int n, int k, List<Entry> current, List<List<Integer>> results) {
         if (n == 0) {
             return results;
         } else {
-            List<List<Integer>> newValues = new ArrayList<>(current);
+            List<Entry> newValues = new ArrayList<>(current);
 
-            for (List<Integer> value : current) {
-                ArrayList<Integer> integers = new ArrayList<>(value);
+            for (Entry entry : current) {
+                ArrayList<Integer> integers = new ArrayList<>(entry.integers);
                 integers.add(n);
 
-                if (integers.size() == k) {
+                if (entry.size + 1 == k) {
                     results.add(integers);
                 } else {
-                    newValues.add(integers);
+                    newValues.add(new Entry(integers, entry.size + 1));
                 }
             }
 

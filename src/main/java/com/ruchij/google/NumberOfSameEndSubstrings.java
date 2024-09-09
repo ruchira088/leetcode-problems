6fe9d551899330e819c4e957057c2cd5e554e75d
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-09-09 23:33:16 +1000

    Optimized solution for NumberOfSameEndSubstrings

--- src/main/java/com/ruchij/google/NumberOfSameEndSubstrings.java
+++ src/main/java/com/ruchij/google/NumberOfSameEndSubstrings.java
@@ -1,31 +1,58 @@
 package com.ruchij.google;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class NumberOfSameEndSubstrings {
     public int[] sameEndSubstringCount(String s, int[][] queries) {
         int[] answer = new int[queries.length];
+        Map<Character, Integer>[] charMappings = mappings(s);
 
         for (int i = 0; i < answer.length; i++) {
             int[] query = queries[i];
-            answer[i] = sameEndSubstringCount(s, query[0], query[1]);
+            answer[i] = sameEndSubstringCount(charMappings, query[0], query[1] + 1);
         }
 
         return answer;
     }
 
-    int sameEndSubstringCount(String s, int start, int end) {
-        int count = 0;
-        String string = s.substring(start, end + 1);
+    Map<Character, Integer>[] mappings(String input) {
+        Map<Character, Integer>[] chars = new Map[input.length() + 1];
+        chars[0] = new HashMap<>();
         HashMap<Character, Integer> characters = new HashMap<>();
 
-        for (int i = 0; i < string.length(); i++) {
-            Character character = string.charAt(i);
+
+        for (int i = 0; i < input.length(); i++) {
+            Character character = input.charAt(i);
             Integer existing = characters.getOrDefault(character, 0);
             characters.put(character, existing + 1);
+            chars[i + 1] = new HashMap<>(characters);
         }
 
-        for (Integer i : characters.values()) {
+        return chars;
+    }
+
+    int sameEndSubstringCount(Map<Character, Integer>[] mappings, int start, int end) {
+        Map<Character, Integer> startChars = mappings[start];
+        Map<Character, Integer> chars = new HashMap<>(mappings[end]);
+
+        for (Map.Entry<Character, Integer> entry : startChars.entrySet()) {
+            int diff = chars.get(entry.getKey()) - entry.getValue();
+
+            if (diff == 0) {
+                chars.remove(entry.getKey());
+            } else {
+                chars.put(entry.getKey(), diff);
+            }
+        }
+
+        return calculate(chars);
+    }
+
+    int calculate(Map<Character, Integer> chars) {
+        int count = 0;
+
+        for (int i : chars.values()) {
             count += (i * (i + 1))/2;
         }
 

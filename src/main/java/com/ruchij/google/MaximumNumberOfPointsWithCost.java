5d9ec1e3aae7b80a779933116ebdb7caff33ab8b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-09-15 21:46:50 +1000

    Memory optimized solution for MaximumNumberOfPointsWithCost

--- src/main/java/com/ruchij/google/MaximumNumberOfPointsWithCost.java
+++ src/main/java/com/ruchij/google/MaximumNumberOfPointsWithCost.java
@@ -2,37 +2,39 @@ package com.ruchij.google;
 
 public class MaximumNumberOfPointsWithCost {
     public long maxPoints(int[][] points) {
-        long[][] maxPoints = new long[points.length][points[0].length];
+        long[] previousRow = new long[points[0].length];
+        long[] currentRow = new long[points[0].length];
         long max = 0;
-        for (int y = 0; y < points.length; y++) {
-            for (int x = 0; x < points[y].length; x++) {
-                long previous = y == 0 ? 0 : maxPoints[y - 1][x];
-                long currentPoints =  previous + points[y][x];
+        for (int[] point : points) {
+            for (int x = 0; x < point.length; x++) {
+                long currentPoints = previousRow[x] + point[x];
 
                 for (int left = 0; x - left >= 0; left++) {
-                    long currentValue = maxPoints[y][x - left];
+                    long currentValue = currentRow[x - left];
                     long newValue = currentPoints - left;
 
                     if (newValue >= currentValue) {
-                        maxPoints[y][x - left] = newValue;
+                        currentRow[x - left] = newValue;
                         max = Math.max(max, newValue);
                     } else {
                         break;
                     }
                 }
 
-                for (int right = 0; right + x < points[y].length; right++) {
-                    long currentValue = maxPoints[y][x + right];
+                for (int right = 0; right + x < point.length; right++) {
+                    long currentValue = currentRow[x + right];
                     long newValue = currentPoints - right;
 
                     if (newValue >= currentValue) {
-                        maxPoints[y][x + right] = newValue;
+                        currentRow[x + right] = newValue;
                         max = Math.max(max, newValue);
                     } else {
                         break;
                     }
                 }
             }
+            previousRow = currentRow;
+            currentRow = new long[points[0].length];
         }
 
         return max;

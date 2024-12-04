2cba6f350dade331b2e1a437a1a1176fa462608a
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-12-04 18:38:10 +1100

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.13.3" cross CrossVersion.full
 
-  lazy val junitJupiterApi = "org.junit.jupiter" % "junit-jupiter-api" % "5.11.0"
+  lazy val junitJupiterApi = "org.junit.jupiter" % "junit-jupiter-api" % "5.11.3"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
 
